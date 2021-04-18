96eb05d32edd5779b4e141f3882a4cfa41f79749
AleRosae <72870813+AleRosae@users.noreply.github.com>
2021-04-18 18:55:16 +0200

    update css

--- Assets/js/get_style.js
+++ Assets/js/get_style.js
@@ -2,9 +2,9 @@
 		  	$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
 	  });
 
-		$("#style1").click(function(){
+		$("#soviet").click(function(){
 			$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
-		  	$('link[href="../Assets/css/style1.css"]').removeAttr('disabled');
+		  	$('link[href="../Assets/css/soviet.css"]').removeAttr('disabled');
 	  });
 		$("#style2").click(function(){
 		$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
