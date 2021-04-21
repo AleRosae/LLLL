7c84fe104f3772434605436aa10d605a90d436d9
valentinacozzi <valentina.cozzi@studio.unibo.it>
2021-04-21 16:52:30 +0200

    skr2

--- Assets/js/get_style.js
+++ Assets/js/get_style.js
@@ -11,3 +11,13 @@
 		$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
 		$('link[href="../Assets/css/style2.css"]').removeAttr('disabled');
 		});  
+
+		$("#style2").click(function(){
+		$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+		$('link[href="../Assets/css/victorian.css"]').removeAttr('disabled');
+		}); 
+
+		$("#style2").click(function(){
+		$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+		$('link[href="../Assets/css/trashy90.css"]').removeAttr('disabled');
+		}); 
