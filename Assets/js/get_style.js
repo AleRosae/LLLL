16e0bdab52450e21341e143ae7824f46d4ac9318
valentinacozzi <valentina.cozzi@studio.unibo.it>
2021-04-22 09:51:49 +0200

    changes

--- Assets/js/get_style.js
+++ Assets/js/get_style.js
@@ -12,12 +12,12 @@
 		$('link[href="../Assets/css/style2.css"]').removeAttr('disabled');
 		});  
 
-		$("#style2").click(function(){
+		$("#victorian").click(function(){
 		$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
 		$('link[href="../Assets/css/victorian.css"]').removeAttr('disabled');
 		}); 
 
-		$("#style2").click(function(){
+		$("#trashy90").click(function(){
 		$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
 		$('link[href="../Assets/css/trashy90.css"]').removeAttr('disabled');
 		}); 
