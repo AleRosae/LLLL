d2b5673da6afb3ebd90d0f9322b39f14bbece3c0
AleRosae <72870813+AleRosae@users.noreply.github.com>
2021-04-23 20:09:26 +0200

    hyperfuture

--- Assets/js/get_style.js
+++ Assets/js/get_style.js
@@ -7,10 +7,17 @@
 		  	$('link[href="../Assets/css/soviet.css"]').removeAttr('disabled');
 	  });
 		
-		$("#style2").click(function(){
+		$("#future").click(function(){
 		$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
-		$('link[href="../Assets/css/style2.css"]').removeAttr('disabled');
+		$('link[href="../Assets/css/hyperfuture.css"]').removeAttr('disabled');
+		FuturePopUp();
 		});  
+
+		function FuturePopUp() {
+  		var popup = document.getElementById("myPopup");
+  		popup.classList.toggle("show");
+
+  		}
 
 		$("#victorian").click(function(){
 		$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
