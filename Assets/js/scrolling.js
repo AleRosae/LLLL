6634e779f4181896991da953c4ae7829ba76074a
Alessandro Rosa <72870813+AleRosae@users.noreply.github.com>
2021-04-25 12:22:08 +0200

    Update scrolling.js

--- Assets/js/scrolling.js
+++ Assets/js/scrolling.js
@@ -1,12 +1,13 @@
-$('#article1_box').scroll(function() {
-    console.log("ciao");
+$('.colonne_variabili').scroll(function() {
+    console.log("ao");
     var currY = $(this).scrollTop();
     var postHeight = $(this).height();
-	var scrollHeight = $('.colonne_variabili').height();
+	var scrollHeight = $('#article1_box').height();
     // Current percentual position
 	var scrollPercent = Math.round((currY / (scrollHeight - postHeight)) * 100);
 
     $('.percentage').text(scrollPercent +"%"  );
     $(".circle").attr("stroke-dasharray", scrollPercent+", 100")
     
+
 });
