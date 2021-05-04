9df2f462269a810c19cfeb910161c00559f26f71
Alessandro Rosa <72870813+AleRosae@users.noreply.github.com>
2021-05-04 15:48:31 +0200

    update

--- Assets/js/get_style.js
+++ Assets/js/get_style.js
@@ -1,3 +1,4 @@
+ $(document).ready(function(){
  		$("#reset").click(function(){
 		  	$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
 	  });
@@ -17,11 +18,11 @@
 		if ($("#popUP").children().length == 0) {
   			$("#popUP").append("<div class=\"popup\"><div class=\"popuptext\" id=\"myPopup\"><h1>Welcome User</h1><p>It looks like you are visiting this website through a very old device that is no longer fully supported.</p><p>You device has been recognize as: Computer Desktop</p><p>It also looks like you are not logged in with you Internet Identity. In order to make the internet a safer place, we strongy recomend you to log in with that Internet Identity that you Social Network has provided to you. Remember that starting from January 1st 2031 an Internet Identity will be mandatory in order to browse any website on search engines.</p> 	<p>Due to you unsupported device many features of this website are not going to be fully functional. This may include:</p><ul><li>Interactive ads</li><li>Compatibily with foldable devices and multiscreen devices</li><li>Instant Social Media sharing</li><li>Augmented and Virtual Reality</li></ul><p>If you want to still visit this page please <span id=\"ClosePop\"  onclick=\"CloseFuturePopUp()\">click here</span></p><p>Thank you.</p></div></div>");
   			$("#Articles").attr("id", "ArticlesHidden")
-  			$("#nightview").attr("class", "fas fa-moon");
+  			$(".nightview").attr("class", "nightview fas fa-moon");
   			}
   		else {
 
-			$("#nightview").attr("class", "");
+			$(".nightview").attr("class", "nightview");
   			}
   		};
 
@@ -32,13 +33,13 @@
 		};
 
 		function NightMode(){
-			if ($("#nightview").attr("class") == "fas fa-moon") {
-					$("#nightview").attr("class", "far fa-moon");
+			if ($(".nightview").attr("class") == "nightview fas fa-moon") {
+					$(".nightview").attr("class", "nightview far fa-moon");
 					$(".ArticleBody").css("background-color", "#333");
 					$(".ArticleBody").css("color", "#ffffff");
  		}
- 			else if ($("#nightview").attr("class") == "far fa-moon") {
- 					$("#nightview").attr("class", "fas fa-moon");
+ 			else if ($(".nightview").attr("class") == "nightview far fa-moon") {
+ 					$(".nightview").attr("class", "nightview fas fa-moon");
  					$(".ArticleBody").css("background-color", "#ffffff");
 					$(".ArticleBody").css("color", "#333");
 
@@ -64,3 +65,5 @@
 			$('link[rel="stylesheet alternative"]').attr('disabled', 'true');
 			$('link[href="../Assets/css/groovy70s.css"]').removeAttr('disabled');
 				}); 
+
+});
