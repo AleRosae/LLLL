71df77d7b0bb80f493d45eee0cebe87efa0f9148
SusannaPinotti <72868258+SusannaPinotti@users.noreply.github.com>
2021-05-14 17:01:03 +0200

    cose

--- Assets/js/get_metadata.js
+++ Assets/js/get_metadata.js
@@ -1,12 +1,38 @@
 $(document).ready(function(){
     $(".callmetadata").click(function(){
-        alert("ciao");
         var meta = this.id;
+        $(".infobox .card-text").empty();
         switch(meta){
             case "pers":
-                $(".pers").each(function(){
-                    console.log($(this).text());
+                var lista_nomi1 =[];
+                var lista_nomi2 =[];
+                var lista_nomi3 =[];
+                $("#article1_box .pers").each(function(){
+                    
+                    if(jQuery.inArray($(this).text(), lista_nomi1)== -1){
+                        lista_nomi1.push($(this).text());
+                    } 
+
                 });
+                $("#article2_box .pers").each(function(){
+                    
+                    if(jQuery.inArray($(this).text(), lista_nomi2)== -1){
+                        lista_nomi2.push($(this).text());
+                    } 
+                });
+                $("#article3_box .pers").each(function(){
+                    
+                    if(jQuery.inArray($(this).text(), lista_nomi3)== -1){
+                        lista_nomi3.push($(this).text());
+                    } 
+                });
+                $("#infobox1 .card-text").append("<ul id=\"elencoNomi1\"></ul>");
+                for(var i=lista_nomi1.length-1; i>=0; i++){
+                    $("#elencoNomi1").append("<li>"+lista_nomi1[i]+"</li>");
+                }
+
+            //case 
+            //case 
         }
         
     });
