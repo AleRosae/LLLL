42d913b8321d9bae3f3ecf203b4a5cb61e37605a
SusannaPinotti <72868258+SusannaPinotti@users.noreply.github.com>
2021-05-14 17:20:13 +0200

    spaccato

--- Assets/js/get_metadata.js
+++ Assets/js/get_metadata.js
@@ -1,39 +1,44 @@
-$(document).ready(function(){
-    $(".callmetadata").click(function(){
+$(document).ready(function () {
+    $(".callmetadata").click(function () {
         var meta = this.id;
+        alert(meta);
         $(".infobox .card-text").empty();
-        switch(meta){
-            case "pers":
-                var lista_nomi1 =[];
-                var lista_nomi2 =[];
-                var lista_nomi3 =[];
-                $("#article1_box .pers").each(function(){
-                    
-                    if(jQuery.inArray($(this).text(), lista_nomi1)== -1){
-                        lista_nomi1.push($(this).text());
-                    } 
 
-                });
-                $("#article2_box .pers").each(function(){
-                    
-                    if(jQuery.inArray($(this).text(), lista_nomi2)== -1){
-                        lista_nomi2.push($(this).text());
-                    } 
-                });
-                $("#article3_box .pers").each(function(){
-                    
-                    if(jQuery.inArray($(this).text(), lista_nomi3)== -1){
-                        lista_nomi3.push($(this).text());
-                    } 
-                });
-                $("#infobox1 .card-text").append("<ul id=\"elencoNomi1\"></ul>");
-                for(var i=lista_nomi1.length-1; i>=0; i++){
-                    $("#elencoNomi1").append("<li>"+lista_nomi1[i]+"</li>");
-                }
+        var lista_nomi1 = [];
+        var lista_nomi2 = [];
+        var lista_nomi3 = [];
+        $("#article1_box ."+meta).each(function () {
 
-            //case 
-            //case 
+            if (jQuery.inArray($(this).text(), lista_nomi1) == -1) {
+                lista_nomi1.push($(this).text());
+            }
+
+        });
+        $("#article2_box ."+meta).each(function () {
+
+            if (jQuery.inArray($(this).text(), lista_nomi2) == -1) {
+                lista_nomi2.push($(this).text());
+            }
+        });
+        $("#article3_box ."+meta).each(function () {
+
+            if (jQuery.inArray($(this).text(), lista_nomi3) == -1) {
+                lista_nomi3.push($(this).text());
+            }
+        });
+        $("#infobox1 .card-text").append("<ul id=\"elencoNomi1\"></ul>");
+        for (i in lista_nomi1) {
+            $("#elencoNomi1").append("<li>" + lista_nomi1[i] + "</li>");
         }
-        
+        $("#infobox2 .card-text").append("<ul id=\"elencoNomi2\"></ul>");
+        for (i in lista_nomi2) {
+            $("#elencoNomi2").append("<li>" + lista_nomi2[i] + "</li>");
+        }
+
+        $("#infobox3 .card-text").append("<ul id=\"elencoNomi3\"></ul>");
+        for (i in lista_nomi3) {
+            $("#elencoNomi3").append("<li>" + lista_nomi3[i] + "</li>");
+        }
+
     });
 });
