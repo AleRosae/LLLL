3ae229a1e3f1f5fe305e4d5e770792d58cc3c170
SusannaPinotti <72868258+SusannaPinotti@users.noreply.github.com>
2021-05-31 15:52:32 +0200

    subsfinder de sticazzi

--- Assets/js/subtitles_finder.js
+++ Assets/js/subtitles_finder.js
@@ -1,13 +1,60 @@
 
 $(document).ready(function(){
-    $("#result").click(function(){
+    $("#result1").click(function(){
+        $("#article1_box").find("h2").each(function(index, h2){
+
+            var id = $(h2).attr("id");
+
+            var title = $(h2).text();
+
+            $("#doot1").append('<a href="#'+id+'">'+title+'</a>');
+        });  
         $("#article1_box").find("h3").each(function(index, h3){
     
             var id = $(h3).attr("id");
             
             var title = $(h3).text();
             
-            $("#doot").append('<a href="#'+id+'">'+title+'</a>');
+            $("#doot1").append('<a href="#'+id+'">'+title+'</a>');
+        
+        });
+    });
+    $("#result2").click(function(){
+        $("#article2_box").find("h2").each(function(index, h2){
+
+            var id = $(h2).attr("id");
+
+            var title = $(h2).text();
+
+            $("#doot2").append('<a href="#'+id+'">'+title+'</a>');
+        });  
+        $("#article2_box").find("h3").each(function(index, h3){
+    
+            var id = $(h3).attr("id");
+            
+            var title = $(h3).text();
+            
+            $("#doot2").append('<a href="#'+id+'">'+title+'</a>');
+            
+        });
+    });
+    $("#result3").click(function(){
+        $("#article3_box").find("h2").each(function(index, h2){
+
+            var id = $(h2).attr("id");
+
+            var title = $(h2).text();
+
+            $("#doot3").append('<a href="#'+id+'">'+title+'</a>');
+        });  
+        $("#article3_box").find("h3").each(function(index, h3){
+    
+            var id = $(h3).attr("id");
+            
+            var title = $(h3).text();
+            
+            $("#doot3").append('<a href="#'+id+'">'+title+'</a>');
+            
         });
     });
 });
