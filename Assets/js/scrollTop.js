bd04d13075fdb3dc2a83aeeaaf3efae7e5a8e346
SusannaPinotti <72868258+SusannaPinotti@users.noreply.github.com>
2021-06-03 11:13:00 +0200

    Delete scrollTop.js

--- Assets/js/scrollTop.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $("#article1_box button").click(function(){
-    $("html, body").animate({
-      scrollTop: $("#Title").offset().scrollTop}, 2000);
-    });
-  });
-    
